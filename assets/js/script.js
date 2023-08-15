901bbcb25aa86c8c801aeb7c390b44ddb47db7bd
sergiofuentesvergara <142352673+sergiofuentesvergara@users.noreply.github.com>
2023-08-15 17:01:14 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    $('#enviarCorreo').click(function () {
+        alert("El correo fue enviado correctamente, Gracias por contactarnos. Nos comunicaremos a la brevedad")
+    });
+
+    $('body').on('dblclick', 'h4', function () {
+        $(this).css("color", 'red');
+    });
+
+    $('.card-title').click(function () {
+        $('.card-text').toggle();
+    });
+});
